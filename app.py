302eb77bc938174a8024f982b982fcd668e8e02b
oliao28 <oywliao@gmail.com>
2024-07-02 18:02:15 +0800

    fix minor errors

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import asyncio
 import affinity_utils  as au
 from dotenv import load_dotenv
 # load_dotenv()  # take environment variables from .env.
-# open_api_key = os.getenv('AFFINITY_API_KEY')
+# open_api_key = os.getenv("OPENAI_API_KEY")
 # tavily_api_key = os.getenv("TAVILY_API_KEY")
 # anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
 # AFFINITY_API_KEY = os.getenv('AFFINITY_API_KEY')
-os.environ["OPENAI_API_KEY"] =  st.secrets["open_api_key"] # Set the OpenAI API key as an environment variable
+os.environ["OPENAI_API_KEY"] =  st.secrets["openai_api_key"] # Set the OpenAI API key as an environment variable
 os.environ["TAVILY_API_KEY"] = st.secrets["tavily_api_key"] # Set the Tavyly API key as an environment variable
 os.environ["ANTHROPIC_API_KEY"]= st.secrets["anthropic_api_key"]
 os.environ["LLM_PROVIDER"]=research_config["llm_provider"]
