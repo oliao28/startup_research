aae9f8f2f837cabe52c2f0e7dc7ea477f700e98d
likelytobelaura <38926647+likelytobelaura@users.noreply.github.com>
2024-08-01 14:52:10 +0800

    Update startup_research.py

--- startup_research.py
+++ startup_research.py
@@ -55,7 +55,7 @@ def combine_reports(prompt, offline, online):
     response = str(completion.choices[0].message.content)
     return response
 
-@st.experimental_singleton
+@st.experimental_memo
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
