a340a613f084723ff4c0e5979c367210596b934a
likelytobelaura <38926647+likelytobelaura@users.noreply.github.com>
2024-08-08 10:41:04 +0800

    Update startup_research.py

--- startup_research.py
+++ startup_research.py
@@ -120,6 +120,8 @@ with st.echo():
 
       if text_content != None and len(text_content) > 1500:
         text_content = text_content[0:1500]
+      else:
+         text_content += page_source[0:1500-len(text_content)]
 
       client = OpenAI()
 
