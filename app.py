d4a1bc60c9b8c79502e2921019b2225a60548bf1
oliao28 <oywliao@gmail.com>
2024-08-08 22:10:43 +0800

    temporarily fall back to openAI b/c anthropic claude outage

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ import affinity_utils  as au
 os.environ["OPENAI_API_KEY"] =  st.secrets["openai_api_key"] # Set the OpenAI API key as an environment variable
 os.environ["TAVILY_API_KEY"] = st.secrets["tavily_api_key"] # Set the Tavyly API key as an environment variable
 os.environ["ANTHROPIC_API_KEY"]= st.secrets["anthropic_api_key"]
-# os.environ["LLM_PROVIDER"]=research_config["llm_provider"]
-# os.environ["FAST_LLM_MODEL"]=research_config["fast_llm_model"]
-# os.environ["SMART_LLM_MODEL"]=research_config["smart_llm_model"]
+os.environ["LLM_PROVIDER"]="openai" #research_config["llm_provider"]
+os.environ["FAST_LLM_MODEL"]="gpt-4o-mini" #research_config["fast_llm_model"]
+os.environ["SMART_LLM_MODEL"]="gpt-4o" #research_config["smart_llm_model"]
 os.environ["DOC_PATH"] = os.path.join("company")
 
 AFFINITY_API_KEY = st.secrets["affinity_api_key"]
