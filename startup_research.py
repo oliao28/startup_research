d14d02fd252381780ebb2379ee72953ff5d2422d
likelytobelaura <38926647+likelytobelaura@users.noreply.github.com>
2024-08-15 11:00:22 +0800

    Update startup_research.py

--- startup_research.py
+++ startup_research.py
@@ -50,9 +50,10 @@ def validate_url(url):
     return url
 
 async def generate_summary(url):
+    sourcelist = [url]
     print("start generating summary")
     prompt = f"Give me a 5 sentence overview of the company found at" + url + " especially what products it offers and its end users, and the industry it operates in. The company "
-    researcher = GPTResearcher(prompt, report_type="custom_report", verbose = True, source_urls=url)
+    researcher = GPTResearcher(prompt, report_type="custom_report", verbose = True, source_urls=sourcelist)
     report = await researcher.write_report()
     return report
 
